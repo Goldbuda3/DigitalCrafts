98b33ab294f9fa521a5c7ce54a79afe0d7c89186
Joaquin Vargas <joaquin.vargas90@gmail.com>
2019-10-26 13:55:21 -0500

    Update

--- dog.js
+++ dog.js
@@ -1,36 +1,47 @@
 async function fetchAllBreeds(){
-    element.value ="Generating Breeds..";
-    let response = await fetch('https://dog.ceo/api/breeds/image/random');
+    let response = await fetch('https://dog.ceo/api/breeds/list/all');
     let responseObject = await response.json();
     console.log(responseObject);
-    renderDoggo(responseObject);
-    let content = renderDoggo(responseObject);
-    element.value = "Fetch All Breeds...";
-    console.log(breeds);
-}
+    let content = dropdownContent(responseObject);
+    document.getElementById('drop-down').innerHTML = content;
+  }
+  function dropdownContent(responseObject){
+    
+    //code here//
+    let allBreeds = Object.keys(responseObject.message)
+    console.log(allBreeds);
+    console.log(allBreeds[0]);
+    let dropDown = '<select id="breed-dropdown">';
+      /// my logic for drop down options 
+      for(var i = 0; i < allBreeds.length ; i++){
+        dropDown += `<option value="${allBreeds[i]}">${allBreeds[i]}</options>`;
+       }
+      
+    dropDown += '</select>';
+    return dropDown;
+  }
+  async function fetchDoggo(element) {
+    element.value = "Generating doggo...";
 
-async function fetchDoggo(element){
-    element.value ="Generating Doggo...";
-    console.log(event)
-    console.log("I'm Here");
-    let response = await fetch('https://dog.ceo/api/breeds/image/random');
+    let dropdownBreed = document.getElementById('breed-dropdown')
+    console.log(dropdownBreed); 
+
+    if(dropdownBreed == null){
+        var response = await fetch('https://dog.ceo/api/breeds/image/random');
+    }else{
+        selectedbreed = dropdownBreed.selectedOptions[0].value;
+        var response = await fetch('https://dog.ceo/api/breed/' + selectedbreed +'/images/random');
+    }
+
     let responseObject = await response.json();
     console.log(responseObject);
-    renderDoggo(responseObject);
     let content = renderDoggo(responseObject);
-
-    // += to add pictures
-    // = to replace pictures
     document.getElementById("content").innerHTML += content;
     element.value = "Fetch Doggo...";
-}
-
-function renderDoggo(responseObject){
-        return `
-        <img src= "${responseObject.message}" style="max-width:300px; max-height:300px"/>
-
-        `
-}
+  }
+  function renderDoggo(responseObject) {
+    return `<img src="${responseObject.message}" style="max-width:300px;"/>`
+  }
     // console.log(JSON.stringify(responseObject));
     // console.log(responseObject.message);
     // console.log(responseObject.status);
